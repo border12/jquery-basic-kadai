e9f4b1ab3c56d87c5aa44c85d37d3e0f9c9d1585
border12 <139112044+border12@users.noreply.github.com>
2023-12-05 14:27:03 +0900

    Add files via upload

---
+++ kadai_004/event.js
@@ -0,0 +1,13 @@
+$(function(){
+  $(window).on({    
+      'scroll':()=>{
+          console.log('scrollイベントが発生しました');
+      }    
+  });
+  
+  });
+  $(window).on({
+      'load':()=>{
+          console.log('loadイベントが発生しました');
+      },
+  });
